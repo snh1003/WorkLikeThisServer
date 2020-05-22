dd1fc4c39fe6306055e0a48f8f897c47a84ac5e8
youngjjjj <>
2020-05-23 03:46:00 +0900

    add app & package.json

--- Services/UserService/app.js
+++ Services/UserService/app.js
@@ -1,21 +1,31 @@
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const app = express()
+/* eslint-disable no-unused-vars */
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-//DB
+const app = express();
+
+// DB
 mongoose.Promise = global.Promise;
-const username = process.env.MONGODB_ADMIN_NAME
-const password = process.env.MONGODB_ADMIN_PWD
+const username = process.env.MONGODB_ADMIN_NAME;
+const password = process.env.MONGODB_ADMIN_PWD;
 
-mongoose.connect(`mongodb://${username}:${password}@localhost:27017/users`, {useNewUrlParser : true})
+mongoose.connect(`mongodb://${username}:${password}@user-db:27017/users`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  auth: { authSource: 'admin' },
+});
 
-//Server
-app.use(express.json())
-app.use(require('./Controllers'))
-app.use(express.urlencoded({ extended: true }))
+// Server
+app.use(cors());
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('connect' + process.env.PORT)
-})
+app.use(express.json());
 
+app.use(require('./Controllers'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 5001, () => {
+  console.log(`connect${process.env.PORT}`);
+});
